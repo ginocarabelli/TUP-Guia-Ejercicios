e61d7fdd642b186ec15268e4f7535c00676f59d2
Gino <412045@tecnicatura.frc.utn.edu.ar>
2024-09-08 23:36:31 -0300

    ejercicios progra u2

---
+++ Ejercicios/Unidad 2/Problema1/Problema1/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Problema1.Models;
+
+namespace Problema1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        static readonly Fecha oFecha = new Fecha()
+        {
+            Numero = 9,
+            Dia = "Lunes",
+            Mes = "Septiembre",
+            Año = 2024
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(oFecha);
+        }
+    }
+}
---
+++ Ejercicios/Unidad 2/Problema1/Problema1/Models/Fecha.cs
@@ -0,0 +1,10 @@
+﻿namespace Problema1.Models
+{
+    public class Fecha
+    {
+        public int Numero { get; set; }
+        public string Dia { get; set; }
+        public string Mes { get; set; }
+        public int Año { get; set; }
+    }
+}
